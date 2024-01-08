2bd82715a0a35e65dbad66de7f71555fb6688171
Stan Makov <85253471+makovstanislav@users.noreply.github.com>
2024-01-08 15:34:50 +0100

    Add feed

--- app/page.js
+++ app/page.js
@@ -1,11 +1,38 @@
-import Image from 'next/image'
-import Map from '../components/map'
-import styles from './page.module.css'
+import Map from '../components/map';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <main >
-      <Map />
+    <main className={styles.main}>
+      <div className={styles.map}>
+        <Map />
+      </div>
+      <div className={styles.feed}>
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <img src='/feed/apartments.jpg' className={styles.image}></img>
+            <div className={styles.priceTag}>700 грн / доба</div>
+          </div>
+          <p className={styles.cardTitle}>Сдам 1к ул Драгоманова</p>
+          <p className={styles.cardSubtitle}>Донецкий район Киев Украина</p>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <img src='/feed/bycicle.jpg' className={styles.image}></img>
+            <div className={styles.priceTag}>700 грн / доба</div>
+          </div>
+          <p className={styles.cardTitle}>Сдам 1к ул Драгоманова</p>
+          <p className={styles.cardSubtitle}>Донецкий район Киев Украина</p>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <img src='/feed/tetris.jpg' className={styles.image}></img>
+            <div className={styles.priceTag}>700 грн / доба</div>
+          </div>
+          <p className={styles.cardTitle}>Сдам 1к ул Драгоманова</p>
+          <p className={styles.cardSubtitle}>Донецкий район Киев Украина</p>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
