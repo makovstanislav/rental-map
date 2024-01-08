1192969ff97396f963077b1214618ee7e0dadc07
Stan Makov <85253471+makovstanislav@users.noreply.github.com>
2024-01-08 13:33:58 +0100

    Stylize markers

--- components/map.jsx
+++ components/map.jsx
@@ -3,12 +3,6 @@ import React, { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-// Configure marker icon path
-L.Icon.Default.imagePath = '/';
-L.Icon.Default.prototype.options.iconUrl = 'leaflet/images/marker-icon.png';
-L.Icon.Default.prototype.options.iconRetinaUrl = 'leaflet/images/marker-icon-2x.png';
-L.Icon.Default.prototype.options.shadowUrl = 'leaflet/images/marker-shadow.png';
-
 export default function Map() {
     useEffect(() => {
         // Initialize the map
@@ -16,11 +10,38 @@ export default function Map() {
             window.myMap = L.map('map').setView([49.0384, 31.4513], 6);
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+                attribution: '© OpenStreetMap contributors'
             }).addTo(window.myMap);
-            L.marker([49.0384, 31.4513]).addTo(window.myMap);
-        }
+
+            // Default icon
+            var defaultIcon = L.icon({
+                iconUrl: 'circle.png', 
+                shadowUrl: 'circle-shadow.png', 
+                iconSize: [25, 25],
+                shadowSize: [30, 30], 
+                iconAnchor: [12, 12], 
+                shadowAnchor: [14, 14] 
+            });
             
+            // Active icon for clicked state
+            var activeIcon = L.icon({
+                iconUrl: 'circle-active.png', 
+                shadowUrl: 'circle-shadow.png', 
+                iconSize: [25, 25],
+                shadowSize: [30, 30], 
+                iconAnchor: [12, 12], 
+                shadowAnchor: [14, 14] 
+            });
+
+            // Create a marker and add it to the map
+            var marker = L.marker([49.0384, 31.4513], { icon: defaultIcon }).addTo(window.myMap);
+
+            // Add click event to the marker
+            marker.on('click', function() {
+                marker.setIcon(activeIcon);
+            });
+        }
+
         // Cleanup function
         return () => {
             if (window.myMap) {
@@ -33,7 +54,6 @@ export default function Map() {
     return (
         <div>
             <div id="map" style={{ height: '600px', width: '100%' }}></div>
-            <div>jfkld</div>
         </div>
     );
 }
