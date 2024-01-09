ec65cc4377a50f2b5fc9f1a9df02a54d46e9a2a9
Stan Makov <85253471+makovstanislav@users.noreply.github.com>
2024-01-09 12:30:17 +0100

    Disable server-side map render

---
+++ components/add-rental-form.jsx
@@ -0,0 +1,89 @@
+'use client'
+import { useState, useRef } from 'react';
+import { db, storage } from '../firebaseClient' 
+import { ref as dbRef, push } from "firebase/database";
+import { ref as storageRef, uploadBytes, getDownloadURL } from 'firebase/storage';
+import LocationPickerMap from './maps/locationPickerMap'
+
+export default function AddRentalForm() {
+    const [title, setTitle] = useState(null);
+    const [price, setPrice] = useState(null);
+    const [imageFile, setImageFile] = useState(null);
+    const [uploading, setUploading] = useState(false);
+    const [location, setLocation] = useState({ X: 50.0384, Y: 31.4513 });
+    const [showMap, setShowMap] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setUploading(true);
+
+        // Upload the image to Firebase Storage
+        const imageRef = storageRef(storage, 'rental_images/' + imageFile.name);
+        try {
+            const snapshot = await uploadBytes(imageRef, imageFile);
+            const downloadURL = await getDownloadURL(snapshot.ref);
+
+            // Submit all data to Firebase Realtime Database
+            const newRentalRef = dbRef(db, 'cards/');
+            const newRental = { image: downloadURL, title, price, location };
+            await push(newRentalRef, newRental);
+            alert('Rental object added successfully');
+        } catch (error) {
+            console.error("Error adding rental object: ", error);
+            alert('Error adding rental object');
+        }
+
+        // Clear the form
+        setTitle('');
+        setPrice('');
+        setImageFile(null);
+        setUploading(false);
+    };
+
+    const handleLocationSelect = (selectedLocation) => {
+        setLocation(selectedLocation);
+        setShowMap(false);
+        console.log(selectedLocation)
+    };
+
+    return (
+        <div>
+            <h2>Add a New Rental Object</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Image</label>
+                    <input 
+                        type="file" 
+                        onChange={(e) => setImageFile(e.target.files[0])} 
+                    />
+                </div>
+                <div>
+                    <label>Title</label>
+                    <input 
+                        type="text" 
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Price</label>
+                    <input 
+                        type="number" 
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                    />
+                </div>
+                <button type="submit" disabled={uploading}>
+                    {uploading ? 'Uploading...' : 'Submit'}
+                </button>
+                <div>
+                    <button type="button" onClick={() => setShowMap(true)}>Find on the Map</button>
+                </div>
+                {showMap && <LocationPickerMap onLocationSelect={handleLocationSelect} />}
+                <button type="submit" disabled={uploading}>
+                    {uploading ? 'Uploading...' : 'Submit'}
+                </button>
+            </form>
+        </div>
+    );
+}
